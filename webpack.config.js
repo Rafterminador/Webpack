a276442bdbba6792d41e4a0288123f095eae2254
fernando <ferrafterminador@gmail.com>
2022-05-19 14:27:44 -0600

    Webpack con Babel y plugin html, con fines educativos

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    // Entry nos permite decir el punto de entrada de nuestra aplicación
+    entry: "./src/index.js",
+    // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+    output: {
+        // path es donde estará la carpeta donde se guardará los archivos
+        // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+        path: path.resolve(__dirname, "dist"),
+        // filename le pone el nombre al archivo final, se le puede poner con un hash el nombre
+        filename: "main.js"
+    },
+    resolve: {
+        // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea, aqui iria svelte o react si utilizaramos
+        extensions: [".js"]
+    },
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader, usa expresiones regulares
+                test: /\.m?js$/,
+                // Use es un arreglo u objeto donde dices que loader aplicaras
+                use: {
+                    loader: "babel-loader"
+                },
+                // Exclude permite omitir archivos o carpetas especificas
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
+        })
+    ]
+}
